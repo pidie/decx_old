46bbb76ce00f2a313ad88c603abfc11400691a15
Pi <pidiegames@gmail.com>
2021-04-17 12:14:54 -0400

    minor updates

--- Assets/ScriptableObjects/Cards/Scripts/Actions/ActionCardObject.cs
+++ Assets/ScriptableObjects/Cards/Scripts/Actions/ActionCardObject.cs
@@ -23,12 +23,21 @@ public abstract class ActionCardObject : CardObject {
     public int actionLevel = 1;
 
     [Header("Damage Information")]
-    public string attribute; //need to make a <T> type Attribute
+    public Attributes attribute;
     public float damage = 0;
     public DamageTypes damageType;
     [Range(1,5)]
     public int range = 1;
-    public Dictionary<DamageTypes, int> damageCounters;
+    
+    [Header("Damage Counters")]
+    public int addAcidCounter = 0;
+    public int addBleedCounter = 0;
+    public int addColdCounter = 0;
+    public int addFireCounter = 0;
+    public int addHolyCounter = 0;
+    public int addMagicCounter = 0;
+    public int addPoisonCounter = 0;
+    public int addShockCounter = 0;
     
     [Header("Splash Damage")]
     [Range(0, 100)]
@@ -36,7 +45,9 @@ public abstract class ActionCardObject : CardObject {
     public float splashDamage;
     public bool splashOnlyInLine = true;
 
-    private void Start() 
+    [Header("Buff/Debuff Information")]
+
+    private void Start()
     {
         cardType = CardType.Action;
     }
